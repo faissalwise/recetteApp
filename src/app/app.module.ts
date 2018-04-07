3e901cbace5d5140d6f72c13039766461297fdbc
faissalwise <ihabritane.faycal@gmail.com>
2018-04-07 13:19:37 +0100

    commit component

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule }  from '@angular/flex-layout';
+import { MatListModule, MatInputModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+
+
+
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
